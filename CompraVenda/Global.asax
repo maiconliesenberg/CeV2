08b7b961d8852cf35ced1ee401844fb2019209b0
maicon andre liesenberg <maiconliesenberg169@gmail.com>
2017-08-15 17:07:58 -0300

    Add project files.

---
+++ CompraVenda/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CompraVenda.MvcApplication" Language="C#" %>
---
+++ CompraVenda/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CompraVenda.Startup))]
+namespace CompraVenda
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
